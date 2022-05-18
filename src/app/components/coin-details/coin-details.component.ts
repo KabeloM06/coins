cec5de71e2b87997f0eea4c7e41d7698e505c21a
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-05-18 09:41:40 +0200

    installed chart.js and ng2-charts

--- src/app/components/coin-details/coin-details.component.ts
+++ src/app/components/coin-details/coin-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 
+import {ChartConfiguration, ChartType} from 'chart.js';
+import {BaseChartDirective} from 'ng2-charts';
+
 @Component({
   selector: 'app-coin-details',
   templateUrl: './coin-details.component.html',
